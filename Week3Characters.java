269f330c7240bd98a887e00c016654c1dcd31135
GabeUrena <54905586+GabeUrena@users.noreply.github.com>
2019-09-11 12:29:38 -0400

    Add files via upload

---
+++ Week3Characters.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Week3Characters {
+   public static void main (String [] args) {
+	   
+System.out.println   (78 + 98);
+System.out.println   ('a' + 'b'); // 176
+System.out.println   ('a' - 'b'); // 195
+System.out.println   ('0' + '0'); // -1
+System.out.println   ('a' + 1); //96
+System.out.println   ('a'); //a
+
+
+int num = 20;
+double dollars = 17.40;
+String name1 = "Christian";
+String name2 = "Alex";
+System.out.printf(" I have %d cars", num);
+System.out.printf(" Maria has %.1f in her pocket", dollars);
+System.out.printf("%n %s is %d years older than %s", name1, num, name2);
+}
+}
