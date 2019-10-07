55bad08e8b8d089c5bcd3cfaf871598c395bb998
GabeUrena <54905586+GabeUrena@users.noreply.github.com>
2019-10-07 12:41:36 -0400

    Add files via upload

---
+++ week6.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+public class week6 {
+public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	
+System.out.println("Enter three numbers to get their products:");
+int a = input.nextInt();
+int b = input.nextInt();
+int c = input.nextInt();
+int product = a * b * c; 
+
+System.out.println("The product of " + a + ", " + b + " and " + c + " is " + product);
+	
+
+System.out.println("\nEnter two integers: ");
+int n = input.nextInt();
+int m = input.nextInt();
+int sum = 0;
+if (m > n) {
+	while (m > n) {
+		m = m - 1;
+		sum = sum + m;
+	}
+	sum = sum - n;
+}
+	if(m < n) {
+	while (m < n) {
+		n = n - 1;
+		sum = sum + n;
+	}
+	sum = sum - m;
+	}
+System.out.println("The sum of the range between the two numbers is " + sum);
+	
+System.out.println("\nEnter a number to get the average of its range to 1:");
+	
+	n = input.nextInt();
+	sum = 0;
+	int n1 = n - 2;
+	
+	for (int i = 1; i <= n; ++i) {
+		n = n - 1;
+		sum = sum + n;
+	}
+	sum = sum - 1;
+	int average = sum / n1;
+System.out.println("The average of the range is " + average);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+} }
